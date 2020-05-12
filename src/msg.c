510cfd01fe89ccd977d232ac44f19356be414e3f
Michael Drake <michael.drake@codethink.co.uk>
2020-05-12 10:11:03 +0100

    Add generic message handling code.
    This is transport agnostic, so it should fork for USB or SPI.

---
+++ src/msg.c
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2020 Codethink Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "error.h"
+#include "util.h"
+#include "bl.h"
+#include "msg.h"
+
+/**
+ * Handle the LED Test message.
+ *
+ * \param[in]  msg  Message to handle.
+ * \return \ref BL_ERROR_NONE on success, or appropriate error otherwise.
+ */
+static enum bl_error bl_msg_led_test(const union bl_msg_data *msg)
+{
+	return bl_led_set(msg->led_test.led_mask);
+}
+
+/**
+ * Handle the Acquisition Setup message.
+ *
+ * \param[in]  msg  Message to handle.
+ * \return \ref BL_ERROR_NONE on success, or appropriate error otherwise.
+ */
+static enum bl_error bl_msg_acq_setup(const union bl_msg_data *msg)
+{
+	return bl_acq_setup(
+			msg->acq_setup.rate,
+			msg->acq_setup.samples,
+			msg->acq_setup.gain);
+}
+
+/**
+ * Handle the Acquisition Start message.
+ *
+ * \param[in]  msg  Message to handle.
+ * \return \ref BL_ERROR_NONE on success, or appropriate error otherwise.
+ */
+static enum bl_error bl_msg_acq_start(const union bl_msg_data *msg)
+{
+	BL_UNUSED(msg);
+
+	return bl_acq_start();
+}
+
+/**
+ * Handle the Acquisition Abort message.
+ *
+ * \param[in]  msg  Message to handle.
+ * \return \ref BL_ERROR_NONE on success, or appropriate error otherwise.
+ */
+static enum bl_error bl_msg_acq_abort(const union bl_msg_data *msg)
+{
+	BL_UNUSED(msg);
+
+	return bl_acq_abort();
+}
+
+/**
+ * Handle the Reset message.
+ *
+ * \param[in]  msg  Message to handle.
+ * \return \ref BL_ERROR_NONE on success, or appropriate error otherwise.
+ */
+static enum bl_error bl_msg_reset(const union bl_msg_data *msg)
+{
+	BL_UNUSED(msg);
+
+	bl_init();
+
+	return BL_ERROR_NONE;
+}
+
+/* Exported function, documented in msg.h */
+enum bl_error bl_msg_handle(const union bl_msg_data *msg)
+{
+	typedef enum bl_error (* bl_msg_handle_fn)(
+			const union bl_msg_data *msg);
+
+	static const bl_msg_handle_fn fns[] = {
+		[BL_MSG_LED_TEST]  = bl_msg_led_test,
+		[BL_MSG_ACQ_SETUP] = bl_msg_acq_setup,
+		[BL_MSG_ACQ_START] = bl_msg_acq_start,
+		[BL_MSG_ACQ_ABORT] = bl_msg_acq_abort,
+		[BL_MSG_RESET]     = bl_msg_reset,
+	};
+
+	if (msg->type >= BL_ARRAY_LEN(fns) || fns[msg->type] == NULL) {
+		return BL_ERROR_BAD_MESSAGE_TYPE;
+	}
+
+	return fns[msg->type](msg);
+}
---
+++ src/msg.h
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2020 Codethink Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef BL_MSG_H
+#define BL_MSG_H
+
+#include "acq.h"
+#include "led.h"
+
+enum bl_error;
+
+/** Message type. */
+enum bl_msg_type {
+	BL_MSG_RESPONSE,
+	BL_MSG_LED_TEST,
+	BL_MSG_ACQ_SETUP,
+	BL_MSG_ACQ_START,
+	BL_MSG_ACQ_ABORT,
+	BL_MSG_RESET,
+	BL_MSG_SAMPLE_DATA,
+	BL_MSG__COUNT,
+};
+
+/** Message data */
+union bl_msg_data {
+	/** Message type. */
+	uint8_t type;
+
+	/** Data for \ref BL_MSG_RESPONSE. */
+	struct {
+		uint8_t  type;        /**< Must be \ref BL_MSG_RESPONSE */
+		uint8_t  response_to; /**< Type of message response is to. */
+		uint16_t reserved;    /**< Unused, must be zero. */
+		uint8_t  error_code;  /**< Result of command. */
+	} response;
+
+	/**
+	 * Data for \ref BL_MSG_LED_TEST.
+	 *
+	 * Mostly intended for debug, this simply turns LEDs on and off.
+	 * While the interface allows multiple LEDs to be on simultaniously,
+	 * it may not be possible to achieve on the hardware, due to power
+	 * limitations.  Setting a value of 0x00 turns all LEDs off.  The
+	 * least significant bit is \ref BL_LED_ID_0.
+	 */
+	struct {
+		uint8_t  type;     /**< Must be \ref BL_MSG_LED_TEST */
+		uint16_t led_mask; /**< One bit per LED. */
+	} led_test;
+
+	/**
+	 * Data for \ref BL_MSG_ACQ_SETUP.
+	 *
+	 * An aquisition will sample from each LED in turn (unless disabled
+	 * by a configuration of zero gain for all photodiodes) for the number
+	 * of samples specified at the specified sampling rate.
+	 */
+	struct {
+		uint8_t  type;     /**< Must be \ref BL_MSG_ACQ_SETUP */
+		uint16_t rate;     /**< Sampling rate in ms. */
+		uint16_t samples;  /**< Number of samples per enabled LED/pd. */
+		/**
+		 * Gain per LED, per photodiode.
+		 *
+		 * A value of zero means the combination isn't recorded.
+		 * If all four gains for an LED are zero, the LED is not
+		 * turned on.
+		 */
+		uint8_t  gain[BL_LED__COUNT][BL_ACQ_PD__COUNT];
+	} acq_setup;
+
+	/** Data for \ref BL_MSG_ACQ_START. */
+	struct {
+		uint8_t type; /**< Must be \ref BL_MSG_ACQ_START */
+	} acq_start;
+
+	/** Data for \ref BL_MSG_ACQ_ABORT. */
+	struct {
+		uint8_t type; /**< Must be \ref BL_MSG_ACQ_ABORT */
+	} acq_abort;
+
+	/** Data for \ref BL_MSG_RESET. */
+	struct {
+		uint8_t type; /**< Must be \ref BL_MSG_RESET */
+	} reset;
+
+	/** Data for \ref BL_MSG_SAMPLE_DATA. */
+	struct {
+		uint8_t  type;     /**< Must be \ref BL_MSG_SAMPLE_DATA */
+		uint8_t  count;    /**< Number of samples in packet. */
+		uint16_t checksum; /**< Checksum for samples. */
+		uint16_t data[];   /**< Sample data for \ref count samples. */
+	} sample_data;
+};
+
+/**
+ * Helper macro to get the size of a given \ref union bl_msg_data member.
+ *
+ * \param[in]  _msg  Union member to get size of.
+ * \return size of given member.
+ */
+#define BL_SIZEOF_MSG(_msg) \
+	(sizeof(((union bl_msg_data *)NULL)->_msg))
+
+/**
+ * Get the byte length of the given message type.
+ *
+ * Note, in the case of \ref BL_MSG_SAMPLE_DATA, this assumes a count of
+ * zero samples in the message.
+ *
+ * \param[in]  type  Message type to get the byte length of.
+ * \return byte length of message type, or zero for invalid type.
+ */
+static inline uint8_t bl_msg_type_to_len(enum bl_msg_type type)
+{
+	static const uint8_t len[BL_MSG__COUNT] = {
+		[BL_MSG_RESPONSE]    = BL_SIZEOF_MSG(response),
+		[BL_MSG_LED_TEST]    = BL_SIZEOF_MSG(led_test),
+		[BL_MSG_ACQ_SETUP]   = BL_SIZEOF_MSG(acq_setup),
+		[BL_MSG_ACQ_START]   = BL_SIZEOF_MSG(acq_start),
+		[BL_MSG_ACQ_ABORT]   = BL_SIZEOF_MSG(acq_abort),
+		[BL_MSG_RESET]       = BL_SIZEOF_MSG(reset),
+		[BL_MSG_SAMPLE_DATA] = BL_SIZEOF_MSG(sample_data),
+	};
+
+	if (type >= BL_MSG__COUNT) {
+		return 0;
+	}
+
+	return len[type];
+}
+
+/**
+ * Helper to get message type, even if we've not "decoded" yet.
+ *
+ * \param[in]  msg  Message data to extract type from.
+ * \return type message.
+ */
+static inline enum bl_msg_type bl_msg_get_type(void *msg)
+{
+	return ((union bl_msg_data *)msg)->type;
+}
+
+/**
+ * Convert to decoded message.
+ *
+ * \param[in]  data  Message data to decode.
+ * \param[in]  len   Length of message data.
+ * \return Cast of `data` to the \ref union bl_msg_data, or NULL if the
+ *         message length did not match type.
+ */
+static inline union bl_msg_data * bl_msg_decode(uint8_t *data, uint16_t len)
+{
+	if (data == NULL || len == 0) {
+		return NULL;
+	}
+
+	if (bl_msg_type_to_len(bl_msg_get_type(data)) != len) {
+		return NULL;
+	}
+
+	return (void *)data;
+}
+
+/**
+ * Handle an incomming message.
+ *
+ * \param[in]  msg  Incomming message to be handled.
+ * \return \ref BL_ERROR_NONE on success, or appropriate error otherwise.
+ */
+enum bl_error bl_msg_handle(const union bl_msg_data *msg);
+
+#endif
