d6cc7d9bafa72e4ae9370ca67ea836374e690cf9
Sam Kirkham <sam.kirkham@codethink.co.uk>
2020-06-08 15:37:59 +0100

    Add LED descriptions and friendly names

--- src/led.h
+++ src/led.h
@@ -22,24 +22,34 @@ enum bl_error;
 
 /** LED identifiers. */
 enum bl_led_id {
-	BL_LED_ID_0,
-	BL_LED_ID_1,
-	BL_LED_ID_2,
-	BL_LED_ID_3,
-	BL_LED_ID_4,
-	BL_LED_ID_5,
-	BL_LED_ID_6,
-	BL_LED_ID_7,
-	BL_LED_ID_8,
-	BL_LED_ID_9,
-	BL_LED_ID_10,
-	BL_LED_ID_11,
-	BL_LED_ID_12,
-	BL_LED_ID_13,
-	BL_LED_ID_14,
-	BL_LED_ID_15,
+	BL_LED_ID_0, /* LED 1, 850nm, IR */
+	BL_LED_ID_1, /* LED 2, 880nm, IR */
+	BL_LED_ID_2, /* LED 3, 940nm, IR */
+	BL_LED_ID_3, /* LED 4, 1040nm, IR */
+	BL_LED_ID_4, /* LED 5, 1200nm, IR */
+	BL_LED_ID_5, /* LED 6, 1450nm, IR */
+	BL_LED_ID_6, /* LED 7, 1550nm, IR */
+	BL_LED_ID_7, /* LED 8, 1650nm, IR */
+	BL_LED_ID_8, /* LED 9, 740nm, DULL_RED */
+	BL_LED_ID_9, /* LED 10, 660nm,  DEEP_RED*/
+	BL_LED_ID_10, /* LED 11, 638nm, LIGHT_RED */
+	BL_LED_ID_11, /* LED 12, 612nm, DEEP_ORANGE */
+	BL_LED_ID_12, /* LED 13, 590nm, LIGHT_ORANGE */
+	BL_LED_ID_13, /* LED 14, 570nm, YELLOW */
+	BL_LED_ID_14, /* LED 15, 528nm, GREEN */
+	BL_LED_ID_15, /* LED 16, 470nm, BLUE */
 	BL_LED__COUNT,
 };
+
+/* Friendly names for visible light LEDs */
+#define LED_VISIBLE_BLUE            BL_LED_ID_15
+#define LED_VISIBLE_GREEN           BL_LED_ID_14
+#define LED_VISIBLE_YELLOW          BL_LED_ID_13
+#define LED_VISIBLE_LIGHT_ORANGE    BL_LED_ID_12
+#define LED_VISIBLE_DEEP_ORANGE     BL_LED_ID_11
+#define LED_VISIBLE_LIGHT_RED       BL_LED_ID_10
+#define LED_VISIBLE_DEEP_RED        BL_LED_ID_9
+#define LED_VISIBLE_DULL_RED        BL_LED_ID_8
 
 /**
  * Initialise the LED module.
