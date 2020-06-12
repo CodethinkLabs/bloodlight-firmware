23b3c059a7e5806f35942103920460d31ecd9e61
Sam Kirkham <sam.kirkham@codethink.co.uk>
2020-06-12 15:39:06 +0100

    Dyaniamically show more graphs with more sources

--- tools/plot.py
+++ tools/plot.py
@@ -9,21 +9,23 @@ import datetime
 import threading
 import time
 import numpy as np
+import math
 
 style.use('ggplot')
 
-max_sources = 4
+max_sources = 8
 max_data_points = 2000
 refreshtime_ms = 100
 killed = False
 
 fig = plt.figure()
-ax = [fig.add_subplot(2,2,i+ 1) for i in range(4)]
+axes = []
+# the data source channel the axis displays
+ax_channel = []
 xs = [[] for i in range(max_sources)]
 ys = [[] for i in range(max_sources)]
 locks = [threading.Lock() for i in range(max_sources)]
 
-buff = ''
 
 def get_data_from_line(line):
     s,x,y = line.split(',')
@@ -32,8 +34,8 @@ def get_data_from_line(line):
 def cat_2d_lists(a, b):
     return [a[i] + b[i] if i < len(b) else a[i] for i in range(len(a))]
 
-def read_new_data_from_stdin():
-    global xs, ys, buff, max_sources, killed, locks
+def read_new_data_from_stdin(xy, ys, max_sources, killed, locks):
+    buff = ''
     while not killed:
         max_new_data_to_read = 500
         data_read = 0
@@ -73,19 +75,53 @@ def read_new_data_from_stdin():
                         time.sleep(0.01)
         time.sleep(0.5)
 
-def animate(i):
+def calc_grid(n):
+    rows = math.ceil(math.sqrt(n + 1/4) - 1/2)
+    cols = math.ceil(math.sqrt(n))
+    return rows, cols
 
-    global xs, ys, locks, killed
 
+def animate(a, xs, ys, max_sources, killed, locks, axes, ax_channel):
     for i in range(max_sources):
-        ax[i].clear()
-        while not killed:
-            if locks[i].acquire(False):
-                ax[i].plot(xs[i], ys[i])
-                locks[i].release()
-                break
-            else:
-                time.sleep(0.01)
+        # find the axes indexes that belongs to this channel, if there is one
+        chan_ax = -1
+        for j, chan in enumerate(ax_channel):
+            if chan == i:
+                chan_ax = j
+        # rearrange and add a new graph if we don't have one for this data source
+        # and the source has data
+        draw = False
+        if chan_ax == -1:
+            if len(xs[i]):
+                temp_ax = []
+                temp_ax_channel = []
+                for ax in axes:
+                    temp_ax.append(ax)
+                    fig.delaxes(ax)
+                for ax_ch in ax_channel:
+                    temp_ax_channel.append(ax_ch)
+                num_graphs = len(axes) + 1
+                rows, cols = calc_grid(num_graphs)
+                axes[:] = []
+                ax_channel[:] = []
+                for j, ax in enumerate(temp_ax):
+                    axes.append(fig.add_subplot(rows, cols, j + 1))
+                    ax_channel.append(temp_ax_channel[j])
+                axes.append(fig.add_subplot(rows, cols, len(temp_ax_channel) + 1))
+                ax_channel.append(i)
+                draw = True
+
+        else:
+            draw = True
+        if draw:
+            axes[chan_ax].clear()
+            while not killed:
+                if locks[i].acquire(False):
+                    axes[chan_ax].plot(xs[i], ys[i])
+                    locks[i].release()
+                    break
+                else:
+                    time.sleep(0.01)
 
 def handle_close(evt):
     global killed
@@ -93,7 +129,7 @@ def handle_close(evt):
 
 
 fig.canvas.mpl_connect('close_event', handle_close)
-ani = animation.FuncAnimation(fig, animate, interval=refreshtime_ms)
-data_thread = threading.Thread(target=read_new_data_from_stdin)
+ani = animation.FuncAnimation(fig, animate, interval=refreshtime_ms, fargs=(xs, ys, max_sources, killed, locks, axes, ax_channel))
+data_thread = threading.Thread(target=read_new_data_from_stdin, args=(xs, ys, max_sources, killed, locks))
 data_thread.start()
 plt.show()
