7e932383c1840eea466679192752f0f5f74c1823
Michael Drake <michael.drake@codethink.co.uk>
2020-06-26 16:07:50 +0100

    Example acquisition script: Update for new interface.

--- run.sh
+++ run.sh
@@ -4,9 +4,27 @@
 
 declare DEVICE="$1"
 
-# Example acquisition parameters:
-# - 50 Hz
-# - 4-bit oversample
-# - Enabling source #1 only
-# - Photodiode gains of 16, 1, 1, and 1
-./tools/bl start "$DEVICE" 50 4 0x01 16 1 1 1
+# Example acquisition:
+
+# Turn on the green LED (1 << 14)
+./tools/bl led        "$DEVICE" 0x4000
+
+# Accumulate 512 readings per sample
+./tools/bl oversample "$DEVICE" 512
+
+# 16x gain for photodiode 1, and the rest without gains
+./tools/bl gains      "$DEVICE" 16 1 1 1
+
+# Channel offsets:
+# - Photodiode 1:  280000
+# - Photodiode 2:  0
+# - Photodiode 3:  100000
+# - Photodiode 4:  0
+# - 3.3 volt line: 240000
+# - 5.0 volt line: 240000
+# - Temperature:   180000
+./tools/bl offset     "$DEVICE" 280000 0 100000 0 240000 240000 180000
+
+# Start an acquisition at 50 Hz with source mask 0x77 (enabling channels
+# 1, 2, 3, 5, 6, and 7).
+./tools/bl start      "$DEVICE" 50 0x77
