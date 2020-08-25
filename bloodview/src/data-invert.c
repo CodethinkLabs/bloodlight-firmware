d8180f2b1ef33dcf20d7fecc02d93bb259934a49
Michael Drake <michael.drake@codethink.co.uk>
2020-08-25 11:02:34 +0100

    bloodview: data-invert: Add data inversion filter.

---
+++ bloodview/src/data-invert.c
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2020 Codethink Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "data-invert.h"
+#include "util.h"
+
+/** Averaging filter context. */
+struct data_invert_ctx {
+	unsigned invert;
+	unsigned count;
+};
+
+/* Exported interface, documented in data-invert.h */
+void data_invert_fini(void *pw)
+{
+	struct data_invert_ctx *ctx = pw;
+
+	free(ctx);
+}
+
+/* Exported interface, documented in data-invert.h */
+void *data_invert_init(
+		const struct data_invert_config *config,
+		unsigned frequency,
+		unsigned channels,
+		uint32_t src_mask)
+{
+	struct data_invert_ctx *ctx;
+	unsigned index;
+
+	BV_UNUSED(frequency);
+
+	ctx = calloc(1, sizeof(*ctx));
+	if (ctx == NULL) {
+		return NULL;
+	}
+
+	index = 0;
+	for (unsigned i = 0; i < BL_ACQ__SRC_COUNT; i++) {
+		if (src_mask & (1u << i)) {
+			if (config->invert[index] == true) {
+				ctx->invert |= (1u << index);
+			}
+			index++;
+		}
+	}
+
+	ctx->count = channels;
+	return ctx;
+}
+
+/* Exported interface, documented in data-invert.h */
+uint32_t data_invert_proc(
+		void *pw,
+		unsigned channel,
+		uint32_t sample)
+{
+	struct data_invert_ctx *ctx = pw;
+
+	assert(channel < ctx->count);
+
+	if (ctx->invert & (1u << channel)) {
+		sample = UINT32_MAX - sample;
+	}
+
+	return sample;
+}
