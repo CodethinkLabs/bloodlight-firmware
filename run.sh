0cb541ed7fed7d29aa4d9856b98bdd7d6bb088d9
Michael Drake <michael.drake@codethink.co.uk>
2020-09-09 15:54:38 +0100

    host script: Update for new host tools interface.
    Also fire an abort message on off command.

--- run.sh
+++ run.sh
@@ -35,17 +35,26 @@ run_cal()
 	# Shine the lights
 	./tools/bl led     "$device" "$led_mask"
 
-	# chancfg <channel> <gain> [offset] [shift] [sample32]
-	./tools/bl chancfg "$device" 0 16  0  0  1 # Photodiode 1
-	./tools/bl chancfg "$device" 1  1  0  0  1 # Photodiode 2
-	./tools/bl chancfg "$device" 2  1  0  0  1 # Photodiode 3
-	./tools/bl chancfg "$device" 3  1  0  0  1 # Photodiode 4
-	./tools/bl chancfg "$device" 4  1  0  0  1 # 3.3V
-	./tools/bl chancfg "$device" 5  1  0  0  1 # 5.0V
-	./tools/bl chancfg "$device" 6  1  0  0  1 # Temperature
+	# srccfg <source> <gain> <offset> <sw oversample> [hw oversample] [hw shift]
+	./tools/bl srccfg "$device" 0 16 0 "$oversample" 0 0 # Photodiode 1
+	./tools/bl srccfg "$device" 1  1 0 "$oversample" 0 0 # Photodiode 2
+	./tools/bl srccfg "$device" 2  1 0 "$oversample" 0 0 # Photodiode 3
+	./tools/bl srccfg "$device" 3  1 0 "$oversample" 0 0 # Photodiode 4
+	./tools/bl srccfg "$device" 4  1 0 "$oversample" 0 0 # 3.3V
+	./tools/bl srccfg "$device" 5  1 0 "$oversample" 0 0 # 5.0V
+	./tools/bl srccfg "$device" 6  1 0 "$oversample" 0 0 # Temperature
+
+	# chancfg <channel> <source> [offset] [shift] [sample32]
+	./tools/bl chancfg "$device" 0 0  0  0  1 # Photodiode 1
+	./tools/bl chancfg "$device" 1 1  0  0  1 # Photodiode 2
+	./tools/bl chancfg "$device" 2 2  0  0  1 # Photodiode 3
+	./tools/bl chancfg "$device" 3 3  0  0  1 # Photodiode 4
+	./tools/bl chancfg "$device" 4 4  0  0  1 # 3.3V
+	./tools/bl chancfg "$device" 5 5  0  0  1 # 5.0V
+	./tools/bl chancfg "$device" 6 6  0  0  1 # Temperature
 
 	# Start the calibration acquisition.
-	./tools/bl start   "$device" "$frequency" "$oversample" "$src_mask"
+	./tools/bl start   "$device" "$frequency" "$src_mask"
 }
 
 # Run an acquisition.
@@ -60,25 +69,35 @@ run_acq()
 	# Shine the lights
 	./tools/bl led     "$device" "$led_mask"
 
-	# chancfg <channel> <gain> [offset] [shift] [sample32]
-	./tools/bl chancfg "$device" 0 16 1264480 0 # Photodiode 1
-	./tools/bl chancfg "$device" 1  1   54879 0 # Photodiode 2
-	./tools/bl chancfg "$device" 2  1  567447 0 # Photodiode 3
-	./tools/bl chancfg "$device" 3  1       0 0 # Photodiode 4
-	./tools/bl chancfg "$device" 4  1 1038856 0 # 3.3V
-	./tools/bl chancfg "$device" 5  1 1031704 0 # 5.0V
-	./tools/bl chancfg "$device" 6  1  860701 0 # Temperature
+	# srccfg <source> <gain> <offset> <sw oversample> [hw oversample] [hw shift]
+	./tools/bl srccfg "$device" 0 16 0 "$oversample" 0 0 # Photodiode 1
+	./tools/bl srccfg "$device" 1  1 0 "$oversample" 0 0 # Photodiode 2
+	./tools/bl srccfg "$device" 2  1 0 "$oversample" 0 0 # Photodiode 3
+	./tools/bl srccfg "$device" 3  1 0 "$oversample" 0 0 # Photodiode 4
+	./tools/bl srccfg "$device" 4  1 0 "$oversample" 0 0 # 3.3V
+	./tools/bl srccfg "$device" 5  1 0 "$oversample" 0 0 # 5.0V
+	./tools/bl srccfg "$device" 6  1 0 "$oversample" 0 0 # Temperature
+
+	# chancfg <channel> <source> [offset] [shift] [sample32]
+	./tools/bl chancfg "$device" 0 0 1264480 0 # Photodiode 1
+	./tools/bl chancfg "$device" 1 1   54879 0 # Photodiode 2
+	./tools/bl chancfg "$device" 2 2  567447 0 # Photodiode 3
+	./tools/bl chancfg "$device" 3 3       0 0 # Photodiode 4
+	./tools/bl chancfg "$device" 4 4 1038856 0 # 3.3V
+	./tools/bl chancfg "$device" 5 5 1031704 0 # 5.0V
+	./tools/bl chancfg "$device" 6 6  860701 0 # Temperature
 
 	# Start the calibration acquisition.
-	./tools/bl start   "$device" "$frequency" "$oversample" "$src_mask"
+	./tools/bl start   "$device" "$frequency" "$src_mask"
 }
 
-# Turn off the lights.
+# Turn off the lights and stop any acquisition.
 run_off()
 {
 	declare device="${DEVICE:-$DEFAULT_DEVICE}"
 
 	./tools/bl led     "$device" 0x0000
+	./tools/bl abort   "$device"
 }
 
 # Sequential exexute calibration and acquisition
@@ -114,7 +133,7 @@ run_cal_acq()
 	rm "$TMP_CFG"
 
 	# Start the calibration acquisition.
-	./tools/bl start   "$device" "$frequency" "$oversample" "$src_mask"
+	./tools/bl start   "$device" "$frequency" "$src_mask"
 }
 
 if [ $# -lt 1 ]; then
