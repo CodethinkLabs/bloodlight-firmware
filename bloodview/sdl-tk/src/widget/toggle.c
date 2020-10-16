c4dabcdf4a2f19594569e3557483590081376981
Michael Drake <michael.drake@codethink.co.uk>
2020-10-16 09:42:12 +0100

    sdl-tk: widget: toggle: Update for bounding rect in render API.

--- bloodview/sdl-tk/src/widget/toggle.c
+++ bloodview/sdl-tk/src/widget/toggle.c
@@ -57,17 +57,20 @@ static void sdl_tk_widget_toggle_destroy(
  * Render an sdl-tk toggle widget.
  *
  * \param[in]  widget  The toggle widget to render.
+ * \param[in]  rect    Bounding rectangle for widget placement.
  * \param[in]  ren     SDL renderer to use.
- * \param[in]  x       X coordinate.
- * \param[in]  y       Y coordinate.
+ * \param[in]  x       X-coordinate for widget placement.
+ * \param[in]  y       Y-coordinate for widget placement.
  */
 static void sdl_tk_widget_toggle_render(
 		struct sdl_tk_widget *widget,
+		const SDL_Rect       *rect,
 		SDL_Renderer         *ren,
 		unsigned              x,
 		unsigned              y)
 {
 	SDL_TK_UNUSED(widget);
+	SDL_TK_UNUSED(rect);
 	SDL_TK_UNUSED(ren);
 	SDL_TK_UNUSED(x);
 	SDL_TK_UNUSED(y);
