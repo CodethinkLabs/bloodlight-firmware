f90011b5ae55a1f4df29f1e3d0db3d815b057c87
Kejia Hu <kejia.hu@codethink.co.uk>
2020-11-09 12:35:05 +0000

    SPI: use NSS pin to indicate LED flash

--- firmware/src/spi.c
+++ firmware/src/spi.c
@@ -133,10 +133,19 @@ static void bl_spi__setup(void)
 	spi_fifo_reception_threshold_8bit(SPI2);
 	SPI_I2SCFGR(SPI2) &= ~SPI_I2SCFGR_I2SMOD;
 	if (bl_spi_mode == BL_ACQ_SPI_DAUGHTER) {
+		/* Software NSS was used for SPI, and GPIO12 is used
+		 * as generic GPIO to indicate LED flash
+		 */
+		gpio_mode_setup(GPIOB, GPIO_MODE_INPUT,
+						GPIO_PUPD_PULLDOWN, GPIO12);
+
 		spi_set_nss_low(SPI2);
 		spi_set_slave_mode(SPI2);
 		spi_enable(SPI2);
 	} else if (bl_spi_mode == BL_ACQ_SPI_MOTHER) {
+		gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT,
+						GPIO_PUPD_PULLDOWN, GPIO12);
+
 		spi_set_nss_high(SPI2);
 		spi_set_master_mode(SPI2);
 		spi_enable(SPI2);
