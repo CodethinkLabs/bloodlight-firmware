bed96e5b8c576185580b264180852fce573e901f
Kejia Hu <kejia.hu@codethink.co.uk>
2020-11-10 12:27:09 +0000

    SPI: optimize the daughter board LED handling
    To reduce the latency for the daughter board to switch LED, we
    cached the next GPIO to toggle, use directly GPIO write instead
    of calling the bl_led_set function, and moved the transmission of
    SPI to be after the LED switch.
    
    Signed-off-by: Kejia Hu <kejia.hu@codethink.co.uk>

--- firmware/src/spi.c
+++ firmware/src/spi.c
@@ -219,8 +219,22 @@ uint8_t bl_spi_receive(void)
 /* Exported function, documented in spi.h */
 void bl_spi_daughter_poll(void)
 {
+	static uint32_t gpioport, old_gpioport;
+	static uint16_t gpio, old_gpio;
+	static bool to_next = false;
+
 	if (SPI_SR(SPI2) & SPI_SR_RXNE) {
+		bl_spi_mode = BL_ACQ_SPI_DAUGHTER;
 		uint8_t led = bl_spi_receive();
-		bl_led_set(1U << led);
+		gpioport = bl_led_get_port(led);
+		gpio = bl_led_get_gpio(led);
+		to_next = true;
+	}
+	if (to_next && gpio_get(GPIOB, GPIO12)) {
+		gpio_clear(old_gpioport, old_gpio);
+		gpio_set(gpioport, gpio);
+		old_gpioport = gpioport;
+		old_gpio = gpio;
+		to_next = false;
 	}
 }
